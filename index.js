40ad6f2cdd9e5ca22826783f149361c9c3f5b50e
KuldeepKumar Purohit <k.purohit.1101@gmail.com>
2022-06-06 21:55:45 +0530

    Sign In Page Created

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function openNav(){
 function closeNav(){
     document.getElementById("mySidenav").style.width="0";
 }
+
+// Sign In
+var modal=document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick=function(event){
+    if(event.target==modal){
+        modal.style.display="none";
+    }
+}
